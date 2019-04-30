8da5c8877b0beb246734eeb6af939ca6a85613a2
GoTing <253413617@qq.com>
2019-04-30 16:09:30 +0800

    Initial commit

---
+++ js/rem.js
@@ -0,0 +1,4 @@
+document.documentElement.style.fontSize=document.documentElement.clientWidth*100/1366+"px";
+window.addEventListener("resize",function(){
+    document.documentElement.style.fontSize=document.documentElement.clientWidth*100/1366+"px";
+},false)
